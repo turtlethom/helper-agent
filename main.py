cf345724c564fedf28c73e170da8ab7f0d008c85
Thomas James <wjamesthomas3@gmail.com>
2025-06-20 17:57:48 -0400

    B: Small tweaks to main.py

--- main.py
+++ main.py
@@ -2,19 +2,26 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 # Setting up Gemini Client
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 arguments = sys.argv
+print(arguments)
 
 # Exit program if argument not provided
 if len(arguments) != 2:
     sys.exit(1)
+
+user_prompt = arguments[1]
+messages = [
+    types.Content(role="User", parts=[types.Part(text=user_prompt)])
+]
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=arguments[1]
+    contents=messages
 )
 
 prompt_token_count, candidates_token_count = response.usage_metadata.prompt_token_count, response.usage_metadata.candidates_token_count
