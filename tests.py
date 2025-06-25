df28b8f13892ca72cb1ae28e9f317295686c8f4b
Thomas James <wjamesthomas3@gmail.com>
2025-06-24 20:47:48 -0400

    E: Add get_file_content, adjust test cases

--- tests.py
+++ tests.py
@@ -1,11 +1,18 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-test_1 = get_files_info("calculator")  # lists 'calculator'
-test_2 = get_files_info("calculator", "calculator/pkg")  # lists 'calculator/pkg'
-test_3 = get_files_info("calculator", "/bin")  # stays the same for out-of-bounds test
-test_4 = get_files_info("calculator", "calculator/../")  # should still trigger the security check!
+# Test Cases for 'get_files_info'
+# test_1 = get_files_info("calculator")  # lists 'calculator'
+# test_2 = get_files_info("calculator", "calculator/pkg")  # lists 'calculator/pkg'
+# test_3 = get_files_info("calculator", "/bin")  # stays the same for out-of-bounds test
+# test_4 = get_files_info("calculator", "calculator/../")  # should still trigger the security check!
 
-print(test_1)
+# Test Cases for 'get_file_content'
+# test_1 = get_file_content("calculator", "lorem.txt")  # Lorem text file test
+test_2 = get_file_content("calculator", "main.py")  # lists 'calculator/pkg'
+test_3 = get_file_content("calculator", "pkg/calculator.py")  # stays the same for out-of-bounds test
+test_4 = get_file_content("calculator", "/bin/cat")  # should still trigger the security check!
+# print(test_1)
 print(test_2)
 print(test_3)
 print(test_4)
